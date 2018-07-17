f1a574ffa818460e340b2e185ac9fb904c56877e
Charles Oliver <charles.oliver@casumo.com>
2018-07-17 11:49:28 +0100

    add styled-components, modern-normalize

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 
+import "modern-normalize/modern-normalize.css";
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
