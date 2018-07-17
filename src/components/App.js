6bfdc0c7a067818556c5941ba6a516aeff93d54e
Charles Oliver <charles.oliver@casumo.com>
2018-07-17 14:54:31 +0100

    add createlink component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import CreateLink from './CreateLink';
 import LinkList from './LinkList';
 
 class App extends Component {
     render() {
-        return <LinkList />;
+        return (
+            <div>
+                <CreateLink />
+                <hr />
+                <LinkList />
+            </div>
+        );
     }
 }
 
