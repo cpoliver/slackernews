b424468d434767f175e5203ad31f30cf83403c87
Charles Oliver <charles.oliver@casumo.com>
2018-07-17 12:27:17 +0100

    add graphql server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-import 'modern-normalize/modern-normalize.css';
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root'),
+);
 registerServiceWorker();
